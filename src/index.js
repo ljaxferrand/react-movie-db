019f74212a9f30ee86f3dcd747a16c14c2de768f
Chris Coulson <chrisjamescoulson@gmail.com>
2021-08-18 11:58:10 -0700

    Added pages, base styles, variables, and mixins

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import '../scss/styles.scss';
+// import './index.css';
+import AppRouter from './routers/AppRouter';
+import './styles/styles.scss';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AppRouter />
   </React.StrictMode>,
   document.getElementById('root')
 );
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -2,31 +2,34 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from "../components/Footer";
 import PageHome from '../pages/PageHome';
+import PagePopular from '../pages/PagePopular';
+import PageTopRated from '../pages/PageTopRated';
+import PageNowPlaying from '../pages/PageNowPlaying';
+import PageUpcoming from '../pages/PageUpcoming';
 import PageAbout from '../pages/PageAbout';
 import PageFavs from '../pages/PageFavs';
 import PageSingleMovie from '../pages/PageSingleMovie';
 
-
 function AppRouter() {
     return (
         <Router>
-        <div className="wrapper">
+        <div className='wrapper'>
             <Header />
                 <main>
-                <Switch>
-                    <Route path='/' exact><PageHome sort='popular' /></Route>
-                    <Route path='/sort/popular' exact><PageHome sort='popular' /></Route>
-                    <Route path='/sort/top-rated' exact><PageHome sort='top_rated' /></Route>
-                    <Route path='/sort/now-playing' exact><PageHome sort='now_playing' /></Route>
-                    <Route path='/sort/upcoming' exact><PageHome sort='upcoming' /></Route>
-                    <Route path='/about'><PageAbout /></Route>
-                    <Route path='/favs'><PageFavs /></Route>
-                    <Route path='/movie/:id'><PageSingleMovie /></Route>
-                </Switch>
+                    <Switch>
+                        <Route path='/' exact><PageHome sort='popular' /></Route>
+                        <Route path='/popular' exact><PagePopular sort='popular' /></Route>
+                        <Route path='/top-rated' exact><PageTopRated sort='top_rated' /></Route>
+                        <Route path='/now-playing' exact><PageNowPlaying sort='now_playing' /></Route>
+                        <Route path='/upcoming' exact><PageUpcoming sort='upcoming' /></Route>
+                        <Route path='/about'><PageAbout /></Route>
+                        <Route path='/favs'><PageFavs /></Route>
+                        <Route path='/movie/:id'><PageSingleMovie /></Route>
+                    </Switch>
                 </main>
             <Footer />
         </div>
-    </Router>
+        </Router>
     )
 }
 
