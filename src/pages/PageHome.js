07e029e6832f31eb1fa0fcccaca33c3214037fb9
ljmacleanferrand <87734454+ljmacleanferrand@users.noreply.github.com>
2021-08-22 16:57:29 -0700

    Updated PageHome.js with Movies component
    Updated PageHome.js with Movies component import and related UseEffect hook

--- src/pages/PageHome.js
+++ src/pages/PageHome.js
@@ -1,9 +1,31 @@
-function PageHome() {
-    return (
-        <div>
-            
-        </div>
-    )
+import { useEffect, useState } from "react";
+import Movies from "../components/Movies";
+import { API_TOKEN } from "../globals/globals";
+
+function PageHome({ sort }) {
+  const [movieData, setMovieData] = useState(null);
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      const res = await fetch(
+        `https://api.themoviedb.org/3/movie/${sort}?&language=en-US&page=1`,
+        {
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            Authorization: "Bearer " + API_TOKEN,
+          },
+        }
+      );
+      let rawMovieData = await res.json();
+      rawMovieData = rawMovieData.results.splice(0, 12);
+      setMovieData(rawMovieData);
+    };
+
+    fetchMovies();
+  }, [sort]);
+
+  return <div>{movieData !== null && <Movies movieData={movieData} />}</div>;
 }
 
-export default PageHome
+export default PageHome;
