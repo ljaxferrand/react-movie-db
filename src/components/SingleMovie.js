e92c54c3b3979e4e5981ae358682969ce17b795e
Chris Coulson <chrisjamescoulson@gmail.com>
2021-08-23 16:48:38 -0700

    Added functionality to single movie pages. Styling still to come.

--- src/components/SingleMovie.js
+++ src/components/SingleMovie.js
@@ -1,8 +1,35 @@
-function SingleMovie() {
+import noPoster from "../images/no-movie-poster.jpg";
+import thumbsUp from "../images/thumbs-up.svg";
+import thumbsDown from "../images/thumbs-down.svg";
+
+function SingleMovie( {movieData} ) {
+    console.log(movieData);
     return (
-        <div>
+        <section className="singleMovie">
             
-        </div>
+            <div className="singleMovieContent">
+
+                <div className="singleMoviePoster">
+                {movieData.poster_path === null ? <img src={noPoster} alt="No poster found" /> :
+                <img src={`https://image.tmdb.org/t/p/original/${movieData.poster_path}`} alt={movieData.title} />}
+                </div>
+
+                <div className="SingleMovieInfo">
+                    <h2>{movieData.title}</h2>
+                    <p>{movieData.overview}</p>
+                    <p>Release Date: {movieData.release_date}</p>
+                    <span className="rating">
+                        {movieData.vote_average > 5 ? (
+                        <img className="thumbs" src={thumbsUp} alt=""></img>
+                        ) : (
+                        <img className="thumbs" src={thumbsDown} alt="" />
+                        )}
+                        {movieData.vote_average * 10 + "%"}
+          </span>
+                </div>
+            </div>
+
+        </section>
     )
 }
 
--- src/pages/PageSingleMovie.js
+++ src/pages/PageSingleMovie.js
@@ -1,8 +1,36 @@
+import SingleMovie from "../components/SingleMovie";
+import { useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { API_TOKEN } from '../globals/globals';
+
 function PageSingleMovie() {
+
+    const [movieData, setMovieData] = useState(null);
+
+    const { id } = useParams();
+
+    useEffect(() => {
+
+        const fetchMovie = async () => {
+
+            const res = await fetch(`https://api.themoviedb.org/3/movie/${id}?language=en-US`, {
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Bearer ' + API_TOKEN
+                }
+            });
+            let rawMovieData = await res.json();
+            setMovieData(rawMovieData);
+        }
+
+        fetchMovie();
+    }, []);
+
     return (
-        <div>
-            
-        </div>
+        <section>
+            {movieData !== null && <SingleMovie movieData={movieData} />}
+        </section>
     )
 }
 
