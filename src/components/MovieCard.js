782f2a586a2e73b5b6bf9796103e1c84d0bb80c9
nicoledawn92 <87670272+nicoledawn92@users.noreply.github.com>
2021-08-24 21:03:29 -0700

    favourites buttons and page

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,9 +1,23 @@
 import { Link } from "react-router-dom";
+import useGlobal from "../store/globalAppState";
+import FavButton from "./FavButton";
 import noPoster from "../images/no-movie-poster.jpg";
 import thumbsUp from "../images/thumbs-up.svg";
 import thumbsDown from "../images/thumbs-down.svg";
 
-function MovieCard({ movieObj }) {
+function MovieCard({ movieObj, isFav }) {
+
+  const globalStateAndglobalActions = useGlobal();
+  const globalActions = globalStateAndglobalActions[1];
+
+  function handleFavClick(addToFav, obj){
+    if(addToFav === true){
+        globalActions.addFav(obj);
+    }else{
+        globalActions.removeFav(obj.id);
+    }   
+}
+
   // funtion to trim summary length
   function truncateSummary(sentence, amount, tail) {
     const words = sentence.split(" ");
@@ -20,7 +34,9 @@ function MovieCard({ movieObj }) {
 
   return (
     <div className="card-container">
+      
       <div className="movie-card">
+      
         <div className="movie-poster">
           {movieObj.poster_path === null ? (
             <img src={noPoster} alt="No poster"></img>
@@ -28,11 +44,18 @@ function MovieCard({ movieObj }) {
             <img
               src={`https://image.tmdb.org/t/p/w500/${movieObj.poster_path}`}
               alt={movieObj.title}
+              
             />
           )}
         </div>
 
         <div className="movie-info">
+        <div className="btn-favourite">
+                {isFav ? 
+                    <FavButton movieObj={movieObj} remove={true} handleFavClick={handleFavClick} /> : 
+                    <FavButton movieObj={movieObj} handleFavClick={handleFavClick} />
+                }
+            </div>
           <h3 className="movie-title">{movieObj.title}</h3>
           <p>Release date: {movieObj.release_date}</p>
 
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,13 +1,34 @@
-import MovieCard from "./MovieCard";
+// Page Home
+  import { appTitle } from "../globals/globals";
+  import { useEffect } from "react";
+  import useGlobal from "../store/globalAppState";
+  import isFav from "../utilities/isFav";
+  import MovieCard from "./MovieCard";
 
-function Movies({ movieData }) {
-    return (
-      <div className="movies-container">
-        {movieData.map((oneMovie, i) => (
-          <MovieCard key={i} movieObj={oneMovie} />
-        ))}
-      </div>
-    );
-  }
-  
-  export default Movies;
+function Movies({movieData}){
+
+	const globalStateAndglobalActions = useGlobal();
+  const globalState = globalStateAndglobalActions[0];
+
+	useEffect(() => {
+		document.title = `${appTitle} - Home`;
+	}, []);
+
+	return (
+		<main>
+			<section>
+				
+				<div className="movies-container">
+					{movieData.map((oneMovie, i) => {
+						return <MovieCard key={i} 
+									   movieObj={oneMovie}
+									   isFav={isFav(globalState.favs, null, oneMovie.id)} />
+					})}
+				</div>
+			</section>
+		</main>
+	);
+
+}
+
+export default Movies;
