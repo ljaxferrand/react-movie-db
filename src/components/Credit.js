bd44fb401b7f77e02f1d36b76ae1d1886d748a1e
ljmacleanferrand <87734454+ljmacleanferrand@users.noreply.github.com>
2021-09-17 12:51:29 -0700

    added lj  portfolio site link

--- src/components/Credit.js
+++ src/components/Credit.js
@@ -1,48 +1,68 @@
-import imdblogo from '../images/imdb-logo.svg'; 
-import movieBanner from '../images/movie-banner.jpeg';
-import bcitLogo from '../images/bcit-logo.png';
-import '../styles/_about.scss'; 
+import imdblogo from "../images/imdb-logo.svg";
+import movieBanner from "../images/movie-banner.jpeg";
+import bcitLogo from "../images/bcit-logo.png";
+import "../styles/_about.scss";
 
 function credit() {
-    return (
-        <div className="about-page-title">
-            <div className="about-banner-container">
-                <img class="about-us-banner-image" src={movieBanner} alt="About us banner" />
-                <h1 className="about-title">About Us</h1>
-                <figure className="quote">
-                    <blockquote >
-                        <p>"The secret to film is that it's an illusion"</p>
-                    </blockquote>
-                    <figcaption>—George Lucas</figcaption>
-                </figure>
-            </div>
-            
-            <div className="credit-logos-container">
-                <div className="item1">
-                    <p className="about-text">
-                    This product uses the TMDb API but is not endorsed or certified by TMDb.</p>
-                </div>
-                <div className="item2">
-                    <a href="https://www.themoviedb.org/"><img class="imdb-logo" src={imdblogo} alt="Logo for IMDB" /></a>
-                </div>
-                <div className="item3">
-                    <a href="https://www.bcit.ca/programs/front-end-web-developer-certificate-full-time-6535cert/"><img class="bcit-logo" src={bcitLogo} alt="logo for BCIT"/></a>
-                </div>
-                <div className="item4">
-                    <p className="about-text">Enjoy browsing your favourite movies on this site. It was made possible by the contributions of the BCIT front-end web development program, its instructors, and of course, a team of dedicated developers.</p>
-                    <div className="bcitstudents-container">
-                    <ul className="bcitstudents-list">
-                        <li><a href="https://chivitran.com">Chivi Tran</a></li>
-                        <li>LJ MacLean-Ferrand</li>
-                        <li><a href="https://coulson.codes">Chris Coulson</a></li>
-                        <li>Nicole Holland</li>
-                    </ul>
-                </div>
-                </div>
-                
-            </div>  
+  return (
+    <div className="about-page-title">
+      <div className="about-banner-container">
+        <img
+          class="about-us-banner-image"
+          src={movieBanner}
+          alt="About us banner"
+        />
+        <h1 className="about-title">About Us</h1>
+        <figure className="quote">
+          <blockquote>
+            <p>"The secret to film is that it's an illusion"</p>
+          </blockquote>
+          <figcaption>—George Lucas</figcaption>
+        </figure>
+      </div>
+
+      <div className="credit-logos-container">
+        <div className="item1">
+          <p className="about-text">
+            This product uses the TMDb API but is not endorsed or certified by
+            TMDb.
+          </p>
         </div>
-    )
+        <div className="item2">
+          <a href="https://www.themoviedb.org/">
+            <img class="imdb-logo" src={imdblogo} alt="Logo for IMDB" />
+          </a>
+        </div>
+        <div className="item3">
+          <a href="https://www.bcit.ca/programs/front-end-web-developer-certificate-full-time-6535cert/">
+            <img class="bcit-logo" src={bcitLogo} alt="logo for BCIT" />
+          </a>
+        </div>
+        <div className="item4">
+          <p className="about-text">
+            Enjoy browsing your favourite movies on this site. It was made
+            possible by the contributions of the BCIT front-end web development
+            program, its instructors, and of course, a team of dedicated
+            developers.
+          </p>
+          <div className="bcitstudents-container">
+            <ul className="bcitstudents-list">
+              <li>
+                <a href="https://chivitran.com">Chivi Tran</a>
+              </li>
+              <li>
+                <a href="https://ljferrand.com">LJ MacLean-Ferrand</a>
+              </li>
+              <li>
+                <a href="https://coulson.codes">Chris Coulson</a>
+              </li>
+              <li>Nicole Holland</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default credit
+export default credit;
